136d99ab82452596a07f076e5749dd86e4d730d7
Jay <jaydwivedi.programmer@gmail.com>
2025-03-25 15:50:28 -0400

    Session 2 Views and Templates

---
+++ blog/Auth_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("ph/",views.print_hello),
+    path("lt/",views.load_template)
+]
--- blog/Auth_app/views.py
+++ blog/Auth_app/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+#logic to print toward the cient side
+def print_hello(request):
+    return HttpResponse("Hello i am djangooooooooooooooooo")
+
+
+def load_template(request):
+    return render(request,"Auth_app/index.html")
