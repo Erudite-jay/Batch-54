b3db6f1fa6c654a6a95bf2f29d962edff11ef0c1
Jay <jaydwivedi.programmer@gmail.com>
2025-03-26 15:34:18 -0400

    Session 3 - Models, ORM, Admin, Shell

--- blog/Auth_app/admin.py
+++ blog/Auth_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Contact,test
 # Register your models here.
+
+# admin.site.register(Contact)
+admin.site.register(test)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+     list_display=["id",'fullname','email','mobile','message']
+     search_fields=["fullname",'email']
+     list_filter=["fullname",'email']
--- blog/Auth_app/models.py
+++ blog/Auth_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    fullname=models.CharField(max_length=100)
+    email=models.EmailField(max_length=60,unique=True)
+    mobile=models.CharField(max_length=10)
+    message=models.TextField(max_length=200)
+
+class test(models.Model):
+    test=models.CharField(max_length=100)
